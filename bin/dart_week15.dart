d0b24995be1dd81e54cdfa82385b9c67bc3a058a
Natsunsn <Natsunan270@gmail.com>
2023-08-24 15:41:44 +0700

    first commit

---
+++ bin/dart_week15.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:dart_week15/list_demo.dart';
+import 'package:dart_week15/sub_program.dart';
+void main(){
+  basic();
+}
+
+void basic(){
+    setDemo();
+    //listDemo();
+    //doWhile2();
+   // doWhile();
+   //whileLoop();
+}
+
+
